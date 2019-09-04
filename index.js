547f458f24e9c99f008d366233aa26d8a280174c
michelml <michmoreau.l@gmail.com>
2019-09-04 15:33:02 -0400

    adjust script

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 const year = new Date().getFullYear();
 const author = process.argv[process.argv.length - 1];
 const license = `
-## MIT License    
+MIT License
+    
 Copyright (c) ${year} ${author}
   
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  
@@ -15,6 +16,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 `;
 
-fs.writeFileSync("./LICENSE.md", license, "utf-8");
+fs.writeFileSync("./LICENSE", license, "utf-8");
 
 process.exit();
